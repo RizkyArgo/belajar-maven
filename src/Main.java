462e2f1c1b9b4b2380afe106ba753df4000b690a
RizkyArgo <rizkiargo@gmail.com>
2025-07-18 14:21:28 +0000

    tes commit maven

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	public static void main(String[] args){
+
+	System.out.println{"Halo World");
+
+}
+
+	public static int tambah(int a,int b){
+		return a+b;
+	}
+}
---
+++ test/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static
+org.junit.jupiter.api.Assertions.assertEquals;
+
+	public class MainTest{
+
+	@Test
+	public void tesTambah() {
+	assertEquals(5,Main.tambah(2,3));
+
+	}
+}
